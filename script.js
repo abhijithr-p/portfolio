c8b1665b47fd73f0a55d10343a0f58b850b147a9
Abhijith R Pillai <abhijithrpillai12345@gmail.com>
2026-04-21 22:39:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -315,7 +315,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Particles representing magical golden sparks (lumos)
-const particleCount = 600; // Dense magical atmosphere
+const particleCount = 6000; // Dense magical atmosphere
 const particles = new THREE.BufferGeometry();
 const particlePositions = new Float32Array(particleCount * 3);
 
@@ -623,5 +623,3 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
-
-
